07103376d76aa9641a9e98365568ad96fcbe86e1
Stasya3581 <stasya3581@gmail.com>
2022-06-18 12:33:14 +0300

    add fail js with class

--- SCRIPT/code.js
+++ SCRIPT/code.js
@@ -1,71 +1,65 @@
 
-let checkList = document.querySelector('.checkList');
-
-
-function checkListAdd (){
-    let newDiv = document.createElement('div');
-    newDiv.classList.add('checkBox');
-    newDiv.innerHTML = `
-        <input type="text" class="addText">`;
-
-    let newButDel = document.createElement('button');
-    newButDel.classList.add('deleteCheck')
-    newButDel.addEventListener('click',  () =>{
-        newDiv.remove();
-    });
-    newDiv.append(newButDel);
-    checkList.append(newDiv);
+class ToDolist {
+    constructor () {
+        this.addBtn = document.querySelector('.buttonADD');
+        this.checkList = document.querySelector('.checkList');
 
     }
 
+    creatListElementDelBtl (elementToRemove){
+        let newButDel = document.createElement('button');
+        newButDel.classList.add('deleteCheck');
+        newButDel.addEventListener('click', ()=>{
+            elementToRemove.remove();
+        })
+        return newButDel;
 
-let addBut = document.querySelector('.buttonADD');
-addBut.addEventListener( 'click', checkListAdd)
+    }
+
+    creatListElement () {
+        let newDiv = document.createElement('div');
+        newDiv.classList.add('checkBox');
+        newDiv.innerHTML = `
+        <input type="text" class="addText">`;
+
+        let newButDel = this.creatListElementDelBtl(newDiv);
+        newDiv.append(newButDel);
+
+        this.checkList.append(newDiv);
+    }
+
+    init (){
+        
+        this.addBtn.addEventListener('click', this.creatListElement);
+        //2.фильтр
+    }
+}
+ 
+let list = new ToDolist()
+list.init();
 
 
 
+// let checkList = document.querySelector('.checkList');
 
 
+// function checkListAdd (){
+//     let newDiv = document.createElement('div');
+//     newDiv.classList.add('checkBox');
+//     newDiv.innerHTML = `
+//         <input type="text" class="addText">`;
 
+//     let newButDel = document.createElement('button');
+//     newButDel.classList.add('deleteCheck')
+//     newButDel.addEventListener('click',  () =>{
+//         newDiv.remove();
+//     });
+//     newDiv.append(newButDel);
+//     checkList.append(newDiv);
 
-
-
-// class EventButtun {
-//     constructor () {
-//         this.filterButt = document.querySelector('.filtersADD');
-//         this.deleteBut = document.querySelector('.deleteCheck');
-//         this.addBut = document.querySelector('.buttonADD');
-//         this.checkList = document.querySelector('.checkList');
-//         this.checkBox = document.querySelector('.checkBox');
 //     }
-//     deleteCheckList () {
-
-//         this.checkBox.remove(); /*удаляет каждый раз первую строку*/
-//                 // !!! см дом дерево надо удалять соседний элемент от этой кнопки наверное
-//         }
-
-//     buttonPressAdd () {
-       
-
-//         addCreat = () => {
-
-//         this.newDiv = document.createElement('div');
-//         newDiv.classList.add('checkBox');
-//         newDiv.innerHTML = `
-//             <input type="text" class="addText">`;
-
-//         this.newButDel = document.createElement('button');
-//         newButDel.classList.add('deleteCheck');
-//         newButDel.addEventListener('click', this.deleteCheckList);
-//         newDiv.append(newButDel)
-
-//         this.checkList.append(newDiv);
-//         }
-    
-//     this.addBut.addEventListener('click', this.addCreat());
-
-//     }
 
 
+// let addBut = document.querySelector('.buttonADD');
+// addBut.addEventListener( 'click', checkListAdd)
 
-// }
